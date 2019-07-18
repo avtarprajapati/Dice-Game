1b5a714068c9fb0a303b939ed86e086a9de04798
avtarprajapati <prajapatiavtar@gmail.com>
2019-07-18 17:05:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,93 @@
+var score, roundScore, activePlayer, gamePlaying;
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+    if (gamePlaying) {
+            // 1. random dice
+        var dice = Math.ceil(Math.random() * 6);
+
+        // display dice
+        var DiceDom = document.querySelector('.dice');
+        DiceDom.style.display = 'block'
+        DiceDom.src = `dice-${dice}.png`;
+
+        // update score if not equal to 1
+        if (dice !== 1) {
+            // add roundScore
+            roundScore += dice;
+            document.querySelector(`#current-${activePlayer}`).textContent = roundScore; 
+        } else {
+            // nextPlayer turn
+            nextPlayer();
+        }
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click',() => {
+
+    if (gamePlaying) {
+        score[activePlayer] += roundScore;
+
+        document.getElementById(`score-${activePlayer}`).textContent = score[activePlayer];
+
+        var winningScore = document.querySelector('.final-score').value;
+        
+        if (winningScore) {
+            winningScore = winningScore
+        } else {
+            winningScore = 100;
+        }
+
+        if (score[activePlayer] >= winningScore) {
+            gamePlaying = false;
+            document.getElementById(`player-${activePlayer}`).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none'
+            document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+            document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+        } else {
+            nextPlayer();
+        }
+    }
+
+});
+
+function nextPlayer() {
+
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    document.querySelector(`.player-0-panel`).classList.toggle('active');
+    document.querySelector(`.player-1-panel`).classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none'
+
+}
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function init() {
+    score = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.getElementById('player-0').textContent = 'Player 1';
+    document.getElementById('player-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector(`.player-0-panel`).classList.remove('active');
+    document.querySelector(`.player-1-panel`).classList.remove('active');
+    // starting player 0 active
+    document.querySelector(`.player-0-panel`).classList.add('active');
+
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+```
+It is Dice game to play this some instruction there to win game.
+```
+
+> Instruction :-
+
+1. This is 2 player game, playing in rounds
+2. In each turn, a player rolls a dice as many times as he wished. Each result get added to his ROUND score.
+3. BUT, if the player rolls a 1, all his ROUND score is lost. After that, It's next player's turn.
+4. And winning score points is by default is 100, unless if not to give it.
+4. The first player to reach Winning Score points On GLOBAL score wins the game.
